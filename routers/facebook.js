4446c577bd0aefa67da3f96b8edb8616910db6ac
blackheart1520 <donghuuhieu1520@gmail.com>
2020-12-11 23:53:43 +0700

    remove redundant import
    Signed-off-by: blackheart1520 <donghuuhieu1520@gmail.com>

--- routers/facebook.js
+++ routers/facebook.js
@@ -1,6 +1,5 @@
 // eslint-disable-next-line new-cap
 const router = require("express").Router();
-const { validateFacebookWebhook } = require("../middlewares/validateFacebookWebhook");
 const {
   verifyCallbackController,
 } = require("../controllers/webhooks/facebook");
