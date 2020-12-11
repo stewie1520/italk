96c2758f85151add0c75bdcf46648ca29ca9e7c8
blackheart1520 <donghuuhieu1520@gmail.com>
2020-12-12 00:52:59 +0700

    Fix text
    Signed-off-by: blackheart1520 <donghuuhieu1520@gmail.com>

--- services/commands/handler.js
+++ services/commands/handler.js
@@ -2,11 +2,11 @@ const constants = require("./constants");
 const { sendFbMessage } = require("../sendFbMessage");
 const { returnSuccess, returnFail } = require("../common/serviceResponse");
 
-const checkForCommand = (message) => Object.values(constants).includes(message);
+const checkForCommand = (text) => Object.values(constants).includes(text);
 
 const handleCommand = async ({ sender, recipient, message }) => {
   try {
-    if (message === constants.START_CONVERSATION) {
+    if (message.text === constants.START_CONVERSATION) {
       await sendFbMessage({ psid: sender.id, message: "Hello" });
     }
 
