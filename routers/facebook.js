7cea9a6363e641d3f178376a6b2c3da454356ace
blackheart1520 <donghuuhieu1520@gmail.com>
2020-12-11 22:05:51 +0700

    Fixed middleware only in webhook received
    Signed-off-by: blackheart1520 <donghuuhieu1520@gmail.com>

--- routers/facebook.js
+++ routers/facebook.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line new-cap
 const router = require("express").Router();
-
+const { validateFacebookWebhook } = require("../middlewares/validateFacebookWebhook");
 const {
   verifyCallbackController,
 } = require("../controllers/webhooks/facebook");
@@ -8,6 +8,6 @@ const {
 const { webhookEventController } = require("../controllers/webhooks/facebook");
 
 router.get("/", verifyCallbackController);
-router.post("/", webhookEventController);
+router.post("/", validateFacebookWebhook, webhookEventController);
 
 module.exports = router;
--- routers/webhook.js
+++ routers/webhook.js
@@ -1,7 +1,6 @@
 // eslint-disable-next-line new-cap
 const router = require("express").Router();
-const { validateFacebookWebhook } = require("../middlewares/validateFacebookWebhook");
 
-router.use("/facebook", validateFacebookWebhook, require("./facebook"));
+router.use("/facebook", require("./facebook"));
 
 module.exports = router;
